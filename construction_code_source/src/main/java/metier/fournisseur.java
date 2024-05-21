aa3e2ac2e491b396b79fc2649e02dd88fa5acf60
najia <najiaknizi199@gmail.com>
2024-05-21 11:26:48 +0100

    cration classe fournisseur

---
+++ construction_code_source/src/main/java/metier/fournisseur.java
@@ -0,0 +1,52 @@
+package metier;
+
+public class fournisseur {
+    // creation les attributs
+
+    private int idf;
+    private String nom;
+    private String mail;
+    private String telephone;
+
+    // creation les constructeurs
+     public fournisseur(){};
+     public  fournisseur(int idf , String nom , String mail, String telephone){
+       this.idf=idf;
+       this.nom=nom;
+       this.mail=mail;
+       this.telephone=telephone;
+}
+
+    // creation Getters et setters
+
+    public void  setIdf(int idf){
+         this.idf=idf;
+    }
+    public void  setNom(String nom){
+         this.nom=nom;
+    }
+    public  void  setMail(String mail){
+         this.mail=mail;
+    }
+    public  void  setTelephone(String telephone){
+         this.telephone=telephone;
+    }
+
+    public int  getIdf(){
+         return idf;
+    }
+    public  String getNom(){
+         return  nom;
+    }
+    public  String getMail(){
+         return  mail;
+    }
+    public  String getTelephone(){
+         return  telephone;
+    }
+
+    @Override
+    public  String toString(){
+         return  " fournisseur : "+idf +" nom  : "+nom + " mail : "+mail + " telephoe "+ telephone + " ";
+    }
+}
