d90fd30adfa8cd7497be19dccdba256c97e3e45b
najia <najiaknizi199@gmail.com>
2024-05-21 11:05:46 +0100

    cration package metier : classe tache

---
+++ construction_code_source/src/main/java/metier/tache.java
@@ -0,0 +1,72 @@
+package metier;
+
+import java.util.Date;
+
+public class tache {
+    // creation les attributs
+
+
+    private  int id_tache;
+    private String description_tache;
+    private  Date date_debut;
+    private Date date_fin;
+    private String status;
+    private int id_projet;
+    // creation les constructeurs
+
+    public  tache(){};
+    public  tache(int id_tache, String description_tache, Date date_debut, Date date_fin , String status , int id_projet){
+        this.id_tache=id_tache;
+        this.description_tache=description_tache;
+        this.date_debut=date_debut;
+        this.date_fin=date_fin;
+        this.status=status;
+        this.id_projet=id_projet;
+    }
+
+    // creation Getters et setters
+
+    public  void setId_tache(int id_tache){
+        this.id_tache=id_tache;
+    }
+    public  void  setDescription_tache(String description_tache){
+        this.description_tache=description_tache;
+    }
+    public void  setDate_debut(Date date_debut){
+        this.date_debut=date_debut;
+    }
+    public  void setDate_fin(Date date_fin){
+        this.date_fin=date_fin;
+    }
+    public  void  setStatus(String status){
+        this.status=status;
+    }
+    public  void  setId_projet(int id_projet){
+        this.id_projet=id_projet;
+    }
+
+
+    public  int getId_tache(){
+        return  id_tache;
+    }
+    public  String getDescription_tache(){
+        return  description_tache;
+    }
+    public  Date getDate_debut(){
+        return  date_debut;
+    }
+    public  Date getDate_fin(){
+        return  date_fin;
+    }
+    public  String getStatus(){
+        return  status;
+    }
+    public  int getId_projet(){
+        return  id_projet;
+    }
+
+    @Override
+    public  String toString(){
+        return  "Tache : "+id_tache+ " + description : "+description_tache +"  Date debut : "+ date_debut +"  Date_fin: "+date_fin+ "  +status "+status+ "  + id_projet "+id_tache +" ";
+    }
+}
